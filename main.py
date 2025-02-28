f0c6a3f46e006ea4e3f969fa92cf8300da6d3086
qynhan <qhan33@uwo.ca>
2025-02-27 20:01:54 -0500

    initialized player sprite

--- main.py
+++ main.py
@@ -1,7 +1,46 @@
-import backend
+# import backend
 from settings import *
+from player import Player
+
+print("Started running main.py")
 
 class Game:
     def __init__(self):
+        print("initializing the game, in Game constructor")
+        # setup
         pygame.init()
-        self.display_surface = pygame.display.set_mode
+        pygame.event.get()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Survivor')
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        # groups
+        self.all_sprites = pygame.sprite.Group()
+
+        # sprites
+        self.player = Player((400, 300), self.all_sprites)
+        print("Completed initialization of the game")
+
+    def run(self):
+        print("Starting run()")
+        while self.running:
+            # dt
+            dt = self.clock.tick() / 1000
+
+            # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            # update
+            self.all_sprites.update(dt)
+
+            # draw
+            self.all_sprites.draw(self.display_surface)
+            pygame.display.update()
+        pygame.quit()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
---
+++ player.py
@@ -0,0 +1,21 @@
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join('images', 'player', 'down', '0.png')).convert_alpha()
+        self.rect = self.image.get_rect(center = pos)
+
+        # movement
+        self.direction = pygame.Vector2(1, 0)
+        self.speed = 500
+
+    def input(self):
+        pass
+
+    def move(self, dt):
+        self.rect.center += self.direction * self.speed * dt
+
+    def update(self, dt):
+        self.input()
+        self.move(dt)
