b5a46f5e14ded15cc1417e6365111eb692688a13
qynhan <qhan33@uwo.ca>
2025-02-28 11:41:08 -0500

    created collision obstace objects

--- code/main.py
+++ code/main.py
@@ -25,15 +25,21 @@ class Game:
         self.setup()
 
         # sprites
-        self.player = Player((400, 300), self.all_sprites, self.collision_sprites)
+        self.player = Player((500, 300), self.all_sprites, self.collision_sprites)
         
 
 
     def setup(self):
         map = load_pygame(join('data', 'maps', 'world.tmx'))
+        for x, y, image in map.get_layer_by_name('Ground').tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)
+
         for obj in map.get_layer_by_name('Objects'):
             CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
-        print(map)
+
+        for obj in map.get_layer_by_name('Collision'):
+            CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
+        
 
     def run(self):
         print("Starting run()")
--- code/sprites.py
+++ code/sprites.py
@@ -1,7 +1,13 @@
 from settings import *
 
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_rect(topleft = pos)
+
 class CollisionSprite(pygame.sprite.Sprite):
     def __init__(self, pos, surf, groups):
         super().__init__(groups)
         self.image = surf
-        self.rect = self.image.get_rect(center = pos)
+        self.rect = self.image.get_rect(topleft = pos)
